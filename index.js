a53b16a85300ef78b2ff0469774532de07f46e58
Jan Michael Auer <mail@jauer.org>
2015-08-02 16:32:43 +1000

    Add socket.io to the boilerplate.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static('web'));
 
-var server = app.listen(8080, function () {
+server.listen(3000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Forest Online listening at http://%s:%s', host, port);
+});
+
+io.on('connection', function(socket) {
+	socket.emit('test', { text: 'initial' });
+	socket.on('foo', function (data) {
+		socket.emit('bar', data);
+	});
 });
