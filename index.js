3b18e03d5090371322416eb79cd30b13b749e260
Jan Michael Auer <mail@jauer.org>
2015-08-03 17:08:18 +1000

    Modernize index.js

--- index.js
+++ index.js
@@ -1,71 +1,32 @@
-var express = require('express');
+import fs from 'fs';
+import {Server} from 'http';
+import {exec} from 'child_process';
+
+import express from 'express';
+import Socket from 'socket.io';
+import less from 'less-middleware';
+import browserify from 'browserify-middleware';
+
+const FOREST_FILE = '/tmp/test.c';
+const FOREST_CMD = __dirname + '/forest/bin/forestwrapper';
+const EXAMPLES_DIR = __dirname + '/examples/';
+
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var less = require('less-middleware');
-var browserify = require('browserify-middleware');
+var server = Server(app);
+var io = Socket(server);
 
 app.use(less(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 app.get('/forest.js', browserify(__dirname + '/client/app.js'));
 app.use('/fonts', express.static(__dirname + '/node_modules/font-awesome/fonts'));
 
-server.listen(3000, function () {
+server.listen(3000, () => {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Forest Online listening at http://%s:%s', host, port);
 });
 
-io.on('connection', function(socket) {
-	socket.emit('test', { text: 'initial' });
-	socket.on('foo', function (data) {
-		socket.emit('bar', data);
-	});
-
-	socket.on('run', function (data) {
-		console.log(data);
-
-		var fs = require('fs');
-		fs.writeFile("/tmp/test.c", data.code , function(err) {});
-
-		socket.emit('forest-running');
-
-		var exec = require('child_process').exec;
-
-		//var cmd = "forest -svcomp -force_run -file /tmp/test.c | awk '{print $5}' | sed -e 's/^R://g' -e 's/^.....//g' -e 's/....$//g'"
-		//var cmd = "set"
-		var cmd = __dirname + "/forest/bin/forestwrapper"
-
-		//var env = process.env,
-			//someVar,
-			//envDup = {},
-			//child_process = require('child_process');
-
-		//for (someVar in env) {
-			//envDup[someVar] = env[someVar];
-		//}
-		//envDup['LLVM_HOME'      ] = '/llvm-2.9';
-		//envDup['FOREST_HOME'    ] = '/media/DATA/delme/forestweb/online-forest/forest/';
-		//envDup['LD_LIBRARY_PATH'] = '/usr/local/lib/';
-		//envDup['PATH'           ] += ':/llvm-2.9/klee/Release+Asserts/bin/';
-		//envDup['PATH'           ] += ':/llvm-2.9/llvm-gcc-4.2-2.9.source/install/bin/';
-		//envDup['PATH'           ] += ':/llvm-2.9/stp/install/bin/';
-		//envDup['PATH'           ] += ':/llvm-2.9/stp/install/bin/';
-		//envDup['PATH'           ] += ':/llvm-2.9/Release+Asserts/bin/';
-		//envDup['PATH'           ] += ':/media/DATA/delme/forestweb/online-forest/forest/bin/';
-		//exec(cmd,{env: envDup},function(error, stdout, stderr) {
-
-		exec(cmd,function(error, stdout, stderr) {
-			if(error){
-				socket.emit('forest-error', { message : 'Error running forest'});
-			} else {
-				console.log({ status : stdout.trim() });
-				var warnings = [{line:10, column:1, message: "Hello"}];
-				socket.emit('forest-success', { status : stdout.trim() === 'TRUE', warnings });
-			}
-		});
-	});
-
+io.on('connection', socket => {
 	socket.emit('example-list', [
 		{ name : 'array_false-unreach-call1'    , category : 'loops'},
 		{ name : 'array_false-unreach-call2'    , category : 'loops'},
@@ -90,10 +51,24 @@ io.on('connection', function(socket) {
 		{ name : 'Fibonacci02'                  , category : 'recursive'}
 	]);
 
+	socket.on('run', ({code}) => {
+		fs.writeFile(FOREST_FILE, code , function(err) {});
 
-	socket.on('get-code', function ({name}) {
-		var fs = require('fs')
-		fs.readFile( 'examples/' + name, 'utf8', function (err, code) {
+		exec(FOREST_CMD, function(error, stdout, stderr) {
+			if (error) {
+				socket.emit('forest-error', { message : 'Error running forest'});
+			} else {
+				console.log({ status : stdout.trim() });
+				var warnings = [{line:10, column:1, message: "Hello"}];
+				socket.emit('forest-success', { status : stdout.trim() === 'TRUE', warnings });
+			}
+		});
+
+		socket.emit('forest-running');
+	});
+
+	socket.on('get-code', ({name}) => {
+		fs.readFile(EXAMPLES_DIR + name, 'utf8', function (err, code) {
 			if (err) {
 				socket.emit('forest-error', { message : 'Example ' + name + ' does not exist!'});
 			} else {
@@ -103,3 +78,4 @@ io.on('connection', function(socket) {
 	});
 
 });
+
